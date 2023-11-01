bdc5fa6725f159fc9e7d9dbc77392691f3271567
VonFrundsberg <maksimau.mikhail@gmail.com>
2023-11-01 13:41:39 +0300

    burgers equation continued

--- misc/burgers_1d_chebyshev_collocation.py
+++ misc/burgers_1d_chebyshev_collocation.py
@@ -9,20 +9,36 @@ n = 200
 chebPoints = spec.chebNodes(n, -1, 1)
 D = spec.chebDiffMatrix(n, -1, 1)
 D2 = D @ D
-dt = 1e-3
+dt = 1e-2
 uPrev = np.ones(n)
 v = 0.01
 t0 = 1.0
 a = 16
+c = 1
 phi = lambda x, t: x/t * (np.sqrt(a/t)*np.exp(-x**2/(4*v*t)))/(1 + np.sqrt(a/t)*np.exp(-x**2/(4*v*t)))
-asol = lambda x, t, c: c + phi((x - c*t), t + t0)
-t = np.linspace(0, 1, 100)
+asol = lambda x, t: c + phi((x - c*t), t + t0)
 
-plt.plot(chebPoints, asol(chebPoints, 0, 1))
-plt.plot(chebPoints, asol(chebPoints, 1, 1))
+plt.plot(chebPoints, asol(chebPoints, 0))
+plt.plot(chebPoints, asol(chebPoints, 1))
 plt.show()
 
-u0_func = lambda x: 
-uNext = nonlin.newtonIterationHybrid(1/2*D, v*D2, 1/2*D + v*D2)
+u0_func = lambda x: asol(x, 0)
+uLeft = lambda t: asol(-1, t)
+uRight = lambda t: asol(1, t)
+uPrev = u0_func(chebPoints)
+# plt.plot(chebPoints, uPrev)
+# plt.show()
+curT = 0
+for i in range(100):
+    uNext = uPrev + (-(uPrev * (D @ uPrev)) + v * (D2 @ uPrev)) * dt
+    curT += dt
+    uNext[0] = uLeft(curT)
+    uNext[-1] = uRight(curT)
+    plt.plot(chebPoints, uNext - asol(chebPoints, curT))
+    plt.show()
+    uPrev = uNext
+
+plt.plot(chebPoints, uNext)
+plt.show()
 
 
